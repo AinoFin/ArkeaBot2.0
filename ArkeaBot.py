54c0106e43662adc6866264bb6fbb235fc518902
Eino Siipi <101316567+EinoFin@users.noreply.github.com>
2024-01-25 12:34:58 +0200

    "it's not good but its good enough to work"

--- ArkeaBot.py
+++ ArkeaBot.py
@@ -1,10 +1,9 @@
-#importit
-import discord
-import xml.etree.ElementTree as ET
-import requests
-from discord.ext import tasks
-import datetime
-from random import randint
+import discord #discord.py
+import xml.etree.ElementTree as ET #ruokalistan pilkkomiseen
+import requests #ruokalistan lataamiseen
+from discord.ext import tasks #ruokataskin loopiin
+import datetime #ajan lomaan mittaamiseen
+from random import randint #randomisoituun reaktioon jälkiruokaan :D
 
 jälkiruuat=["Korvapuusti","Pikkupulla","Omenavanukas","Pannukakku","Vadelmamunkki","Marjakimara","Mehevä omenapiirakka"]
 
@@ -17,6 +16,7 @@ intents.dm_messages = True #turha, dmiä varten
 #bot client
 client = discord.Client(intents=intents)
 
+#lista Guildeista (palvelimista) jolle laitetaan listoja
 Guildlista={}
 class ruokaGuild:
     def __init__(self, listaId, ruokala, arviointi,rooli) -> None:
@@ -25,20 +25,20 @@ class ruokaGuild:
       self.arviointi=arviointi
       self.rooli=rooli
 
-def MitäSaisiOlla(uusiUrl):
-  file = requests.get(uusiUrl,verify=False)
-  filestr=(str(file.content))
-  listid=filestr.find("Rss.aspx?Id=")
-  iidee=filestr[listid+12:filestr.find("&",listid)]
-  ruokala=uusiUrl[59:uusiUrl.find("/",59)]
+def MitäSaisiOlla(uusiUrl): #urlstä kaarea id ja ruokalanimi etsijä
+  file = requests.get(uusiUrl,verify=False) #false koska kaarean sivu on paska
+  filestr=(str(file.content)) 
+  listid=filestr.find("Rss.aspx?Id=") #etitään id
+  iidee=filestr[listid+12:filestr.find("&",listid)] #otetaan id
+  ruokala=uusiUrl[59:uusiUrl.find("/",59)] #otetaan ruokalan nimi
   return iidee, ruokala
 
-def lueguildlist(tiedosto):
+def lueguildlist(tiedosto): #luetaan Guilds.txt alussa jotta saadaan rekisteröidyt ruokalat takaisin
    f=open(tiedosto,"r")
-   for ruokalat in f.read().splitlines():
+   for ruokalat in f.read().splitlines(): #yksi ruokala per rivi
       j=ruokalat.split(" ")
-      guildi=ruokaGuild(j[1],j[2],eval(j[3]),j[4])
-      Guildlista[int(j[0])]=guildi
+      guildi=ruokaGuild(j[1],j[2],eval(j[3]),j[4]) #1=kaareaid, 2=kaarea ruokala, 3=arviointi, 4=pingattava rooli
+      Guildlista[int(j[0])]=guildi #0=discord kanavan id
 
 #ottaa ruuat arkean sivulta ja ottaa turhat pätkät pois arkean sivun tiedostosta ja palauttaa kaikki ruuat ja pääruuat
 viikonRuuat = {}
@@ -66,34 +66,33 @@ def takeAway(listaId,ruokala):
     viikonRuuat[paiva] = [ruokaLyhyt, kasvisLyhyt] #viikon kaikki pääruuat ja niiden merkit (tällä hetkellä ei käytössä)
     kaikkiRuoka[paiva] = [ruokaIso, kasvisIso] #viikon kaikki ruuat, ei vaa pääruuat
 
-    if "Uunimakkara" in kaikkiRuoka[paiva][0]:
+    if "Uunimakkara" in kaikkiRuoka[paiva][0]: #UUNIMAKKARACHECK, TÄRKEIN OSA KOODIA
       kaikkiRuoka[paiva][0]=kaikkiRuoka[paiva][0].replace("Uunimakkara","UUUNIMAKKAARAAAAOUUUUYHJEAAAAHBOYYYYYYY")
   return viikonRuuat,kaikkiRuoka
     #kaikkiRuoka["ma"][0] on Maanantai normiruoka
     #kaikkiRuoka["ti"][1] on Tiistai kasvisruoka
     #päivina ma-pe ja joka päivälle kasvis ja normi ruoka
 
-timez=datetime.timezone(datetime.timedelta(hours=2))
-time=datetime.time(hour=7,tzinfo=timez)
+timez=datetime.timezone(datetime.timedelta(hours=2)) #suomi on +2 aikavyöhykkeellä
+time=datetime.time(hour=7,tzinfo=timez) #aamuseiskalta ruokalistat
 
-@tasks.loop(time=time)
+@tasks.loop(time=time) #ruokataskloop
 async def uusiruokaTask():
-    if datetime.date.today().weekday()==0:
-      ruokaviestit.clear()
-      for i in Guildlista:
+    if datetime.date.today().weekday()==0: #jos on maanantai
+      ruokaviestit.clear() #vanhat viestit pois
+      for i in Guildlista: #jokaseen guildiin viesti
         await viikonlistaviesti(i)
 
 #login ja asettaa aktiviteetin (ja palauttaa ruuat)
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="ruokalistoja"))
-    lueguildlist("Guilds.txt")
-    uusiruokaTask.start()
+    print(f'Logged in as {client.user}') #ei periaatteessa tarvis mutta on mukava olla :)
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="ruokalistoja")) #suomeksi kääntyy "katsotaan ruokalistoja"
+    lueguildlist("Guilds.txt") #luetaan guildit
+    uusiruokaTask.start() #aloitetaan viikottaisten viestien lähettäminen
 
+#ruokaviesti dict ja class
 ruokaviestit = {}
-
-#ruokaviesti class
 class Viesti:
    def __init__(self) -> None:
       self.keskiarvo = 0 #keskiarvo, summa ja jakaja nollassa alussa
@@ -130,11 +129,11 @@ päivät={ #päivät + mitä se tarkoittaa
 async def viikonlistaviesti(lähetysid): #viikonlista funktiona jotta voi lähettää automaattisesti
         kanava=client.get_channel(lähetysid) #kanavaa tarvitaan viestin lähettämiseen
         try:
-           guil=Guildlista[lähetysid]
+           guil=Guildlista[lähetysid] #jos kanavaa ei ole jo
         except:
            await kanava.send("Kanavaa ei ole rekisteröity!")
            return
-        await kanava.send(guil.rooli)
+        await kanava.send(guil.rooli) #lähetetään ping erikseen jotta onni on iso emoji
         embed=discord.Embed(color=0x76b52f) #värin vaihto + embed defination
         embed.set_thumbnail(url="https://media.discordapp.net/attachments/969179339675541515/1062375756568743956/Arkealogo.png") #arkealogo
         takeAway(guil.listaId,guil.ruokala)
@@ -166,8 +165,8 @@ async def viikonlistaviesti(lähetysid): #viikonlista funktiona jotta voi lähet
               case 4:
                   reaktio="<:malsionilonen:921328363581341696>"
             embed.add_field(name=tieto, value=reaktio, inline=True) #lisätään jälkiruokafield
-          else:                      #jos jälkiruokaa ei ole, 
-            embed.add_field(name="", value="", inline=True) #lisätään tyhjä field 
+          else:
+            embed.add_field(name="", value="", inline=True) #lisätään tyhjä field jos jälkiruokaa ei ole 
 
         if guil.arviointi==True: #jos lähetetään arvioitavalle
           embed.set_footer(text=0) #keskiarvo aluksi nolla
@@ -189,24 +188,23 @@ async def viikonlistaviesti(lähetysid): #viikonlista funktiona jotta voi lähet
 #on message, kun saa viestin
 @client.event
 async def on_message(message):
-      #viikonlista komento
-      if message.content.startswith('!viikonlista'): 
+      if message.content.startswith('!viikonlista'): #viikonlista komento
         await viikonlistaviesti(message.channel.id) #lähettää viestin
       
       if message.content.startswith('!lisääruokala'):
           try: 
-            Guildlista[message.channel.id]
+            Guildlista[message.channel.id] #jos guildia ei ole tulee virhe (täs kohtaa halutaan virhe)
           except:
-             pass
+             pass #jatketaan koodin suorittamista
           else:
-            await message.channel.send("Ruokala on jo rekisteröity!")
+            await message.channel.send("Ruokala on jo rekisteröity!") #jos ei anna vihettä nii guild on jo, joten ei lisätä sitä
             return
-          splitmessage=message.content.split(" ")
+          splitmessage=message.content.split(" ") #jaetaan viesti
           uusiID,uusiRuokala=MitäSaisiOlla(str(splitmessage[1]))
           uusiArviointi=eval(splitmessage[2])
-          uusguild = ruokaGuild(uusiID,uusiRuokala,uusiArviointi,splitmessage[3])
-          Guildlista[message.channel.id]=uusguild
-          guildTiedosto=open("Guilds.txt","a")
+          uusguild = ruokaGuild(uusiID,uusiRuokala,uusiArviointi,splitmessage[3]) #tehdään uusi ruokaGuild objekti
+          Guildlista[message.channel.id]=uusguild #lisätään ruokaGuild listaan
+          guildTiedosto=open("Guilds.txt","a") #kirjoitetaan ruokaGuild tiedostoon (a, jotta päästään tiedoston loppuun)
           guildTiedosto.write(str(message.channel.id)+" "+uusiID+" "+uusiRuokala+" "+str(uusiArviointi)+" "+splitmessage[3]+"\n")
           guildTiedosto.close()
           await message.channel.send("Ruokala lisätty!")
@@ -214,51 +212,48 @@ async def on_message(message):
 #käyttäjä lisää reaktion
 @client.event
 async def on_reaction_add(reaction, user):
-    embeds = reaction.message.embeds #ottaa viestin embedit
-    if embeds: #jos embedejä on nii ottaa ekan
-      embed = embeds[0]
-    else:
-      return #muuten cancelaa
     if reaction.message.id not in ruokaviestit: # tarkistaa onko viesti ruokaviesti olemassa
       return #jos ei oo olemassa cancelaa
+    embeds = reaction.message.embeds #ottaa viestin embedit
+    embed = embeds[0]
     viesti: Viesti = ruokaviestit[reaction.message.id] #ottaa viestin mihin reagoitiin
-    if reaction.emoji == "0️⃣" and not user.bot: #emojin + onko käyttäjä botti check
-      await lisääkeskiarvo(viesti,reaction,embed,0)
-    elif reaction.emoji == "1️⃣" and not user.bot:
-      await lisääkeskiarvo(viesti,reaction,embed,1)
-    elif reaction.emoji == "2️⃣" and not user.bot:
-      await lisääkeskiarvo(viesti,reaction,embed,2)
-    elif reaction.emoji == "3️⃣" and not user.bot:
-      await lisääkeskiarvo(viesti,reaction,embed,3)
-    elif reaction.emoji == "4️⃣" and not user.bot:
-      await lisääkeskiarvo(viesti,reaction,embed,4)
-    elif reaction.emoji == "5️⃣" and not user.bot:
-      await lisääkeskiarvo(viesti,reaction,embed,5)
-
+    if not user.bot:
+      match reaction.emoji:
+        case "0️⃣":
+            await lisääkeskiarvo(viesti,reaction,embed,0)
+        case "1️⃣":
+            await lisääkeskiarvo(viesti,reaction,embed,1)
+        case "2️⃣":
+            await lisääkeskiarvo(viesti,reaction,embed,2)
+        case "3️⃣":
+            await lisääkeskiarvo(viesti,reaction,embed,3)
+        case "4️⃣":
+            await lisääkeskiarvo(viesti,reaction,embed,4)
+        case "5️⃣":
+            await lisääkeskiarvo(viesti,reaction,embed,5)
 
 #käyttäjä poistaa reaktion
 @client.event
 async def on_reaction_remove(reaction, user):
-    embeds = reaction.message.embeds #samat ku lisääkeskiarvo :D
-    if embeds:
-      embed = embeds[0]
-    else:
-      return
     if reaction.message.id not in ruokaviestit:  # tarkistaa onko viesti ruokaviesti olemassa
       return
+    embeds = reaction.message.embeds #samat ku lisääkeskiarvo :D
+    embed = embeds[0]
     viesti: Viesti = ruokaviestit[reaction.message.id]
-    if reaction.emoji == "0️⃣" and not user.bot:
-      await poistakeskiarvo(viesti,reaction,embed,0)
-    elif reaction.emoji == "1️⃣" and not user.bot:
-      await poistakeskiarvo(viesti,reaction,embed,1)
-    elif reaction.emoji == "2️⃣" and not user.bot:
-      await poistakeskiarvo(viesti,reaction,embed,2)
-    elif reaction.emoji == "3️⃣" and not user.bot:
-      await poistakeskiarvo(viesti,reaction,embed,3)
-    elif reaction.emoji == "4️⃣" and not user.bot:
-      await poistakeskiarvo(viesti,reaction,embed,4)
-    elif reaction.emoji == "5️⃣" and not user.bot:
-      await poistakeskiarvo(viesti,reaction,embed,5)
+    if not user.bot:
+      match reaction.emoji:
+        case "0️⃣":
+            await poistakeskiarvo(viesti,reaction,embed,0)
+        case "1️⃣":
+            await poistakeskiarvo(viesti,reaction,embed,1)
+        case "2️⃣":
+            await poistakeskiarvo(viesti,reaction,embed,2)
+        case "3️⃣":
+            await poistakeskiarvo(viesti,reaction,embed,3)
+        case "4️⃣":
+            await poistakeskiarvo(viesti,reaction,embed,4)
+        case "5️⃣":
+            await poistakeskiarvo(viesti,reaction,embed,5)
 
 #kirjautuu bottiin tokenilla (katso token.txt)
 client.run(open("token.txt","r").read())
